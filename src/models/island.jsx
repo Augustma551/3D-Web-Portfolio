68913227650a907cb3ea321a8d05d0250a16a9f4
Augustma551 <pirateking555@Els-MacBook-Air.local>
2024-05-25 22:15:04 +0700

    third commit

--- src/models/island.jsx
+++ src/models/island.jsx
@@ -4,7 +4,7 @@ import { useFrame, useThree } from "@react-three/fiber";
 import { a } from '@react-spring/three';
 import islandScene from '../assets/3d/low_poly_flying_island.glb';
 
-const island = ({ isRotating, setIsRotating,setCurrentStage, ...props }) => {
+const Island = ({ isRotating, setIsRotating,setCurrentStage, ...props }) => {
   const islandRef = useRef();
 
 
@@ -821,4 +821,4 @@ return (
   
 }
 
-export default island;
+export default Island;
