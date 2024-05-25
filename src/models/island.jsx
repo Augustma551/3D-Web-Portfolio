3d3f9b0d009b5a8e132ec28f31b99d4d01a38a96
Augustma551 <pirateking555@Els-MacBook-Air.local>
2024-05-25 22:07:18 +0700

    second commit

--- src/models/island.jsx
+++ src/models/island.jsx
@@ -1,9 +1,7 @@
 import { useRef, useEffect } from "react";
 import { useGLTF } from "@react-three/drei";
-
 import { useFrame, useThree } from "@react-three/fiber";
 import { a } from '@react-spring/three';
-
 import islandScene from '../assets/3d/low_poly_flying_island.glb';
 
 const island = ({ isRotating, setIsRotating,setCurrentStage, ...props }) => {
